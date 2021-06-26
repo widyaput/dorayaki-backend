da3117be43fbf29fcfc5c2fe57bb91cce5a6a44a
widyaput <wnugrah.putra@gmail.com>
2021-06-26 22:17:54 +0700

    prepare for merge

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module dorayaki
 go 1.16
 
 require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/go-chi/cors v1.2.0
+	github.com/go-chi/render v1.0.1 // indirect
 	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
--- internal/models/dorayaki.go
+++ internal/models/dorayaki.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 // Dorayaki represents dorayaki in database.
 type Dorayaki struct {
 	ID        int64  `gorm:"primaryKey;autoIncrement"`
@@ -13,4 +19,21 @@ type Dorayaki struct {
 // TableName returns table's name inside database.
 func (Dorayaki) TableName() string {
 	return "dorayaki"
+}
+
+func (d *Dorayaki) Bind(r *http.Request) error {
+	if d.Rasa == "" {
+		return fmt.Errorf("rasa is required")
+	}
+	if d.Deskripsi == "" {
+		return fmt.Errorf("deskripsi is required")
+	}
+	if _, err := url.ParseRequestURI(d.ImageURL); err != nil {
+		return fmt.Errorf("imageurl should be valid")
+	}
+	return nil
+}
+
+func (*Dorayaki) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
